c8d5c80d2a3654e4140d2be4a7a268ec6c9326bb
therisingtsun <77495103+therisingtsun@users.noreply.github.com>
2021-09-16 08:34:58 +0530

    switched to dummy firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,8 @@
 export default {
-	apiKey: "AIzaSyAVWTTGd7zygvPrCpCIvWeJ0k-3iU7FVM0",
-	authDomain: "dv-chitragupta.firebaseapp.com",
-	projectId: "dv-chitragupta",
-	storageBucket: "dv-chitragupta.appspot.com",
-	messagingSenderId: "171315346627",
-	appId: "1:171315346627:web:25b485eaac6a3819fe7676"
+	apiKey: "test",
+	authDomain: "test",
+	projectId: "test",
+	storageBucket: "default-bucket",
+	messagingSenderId: "test",
+	appId: "test"
 };
