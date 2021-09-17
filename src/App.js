bc152526413d1d728543f3d3848c26fc7a7cfa68
Risabh Rai <risabhrai2001@gmail.com>
2021-09-17 15:08:26 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ print(math.floor(102.111))
 `;
 
 export default function App() {
-  const [text, setText] = useState(`
+    const [text, setText] = useState(`
 # Henlo worldo!
 	
 **bold**
@@ -67,172 +67,172 @@ print(math.floor(102.111))
 \`\`\`
 
 `);
-  const data = useRef();
-  const changeHandler = (value) => {
-    setText(value);
-  };
+    const data = useRef();
+    const changeHandler = (value) => {
+        setText(value);
+    };
 
-  function transformer(config) {
-    const editor = data.current.editor;
-    const cursorPosition = editor.listSelections();
-    console.log("cursor pos ", cursorPosition);
+    function transformer(config) {
+        const editor = data.current.editor;
+        const cursorPosition = editor.listSelections();
+        console.log("cursor pos ", cursorPosition);
 
-    const nullCheck = checkEmptySpaceSelection(editor);
+        const nullCheck = checkEmptySpaceSelection(editor);
 
-    if (!nullCheck) {
-      editor.setSelections(selectEnclosingWords(editor));
+        if (!nullCheck) {
+            editor.setSelections(selectEnclosingWords(editor));
+        }
+
+        let prevData = editor.getSelections();
+        let changedData = editor
+            .getSelections()
+            .map((text) => config.transform(text));
+        editor.replaceSelections(changedData);
+
+        let formatted = prevData[0].length < changedData[0].length;
+        modifyPostTransformCursorPos(config, formatted);
+
+        cursorPlacement(editor, nullCheck, config, cursorPosition);
+
+        setFocusOnEditor(editor);
     }
 
-    let prevData = editor.getSelections();
-    let changedData = editor
-      .getSelections()
-      .map((text) => config.transform(text));
-    editor.replaceSelections(changedData);
+    function checkEmptySpaceSelection(editor) {
+        const cursorPosition = editor.getCursor();
+        const check = editor.findWordAt(cursorPosition);
 
-    let formatted = prevData[0].length < changedData[0].length;
-    modifyPostTransformCursorPos(config, formatted);
-
-    cursorPlacement(editor, nullCheck, config, cursorPosition);
-
-    setFocusOnEditor(editor);
-  }
-
-  function checkEmptySpaceSelection(editor) {
-    const cursorPosition = editor.getCursor();
-    const check = editor.findWordAt(cursorPosition);
-
-    let nullCheck;
-    if (!editor.somethingSelected()) {
-      nullCheck =
-        cursorPosition.ch === check.anchor.ch ||
-        cursorPosition.ch === check.head.ch;
+        let nullCheck;
+        if (!editor.somethingSelected()) {
+            nullCheck =
+                cursorPosition.ch === check.anchor.ch ||
+                cursorPosition.ch === check.head.ch;
+        }
+        return nullCheck;
     }
-    return nullCheck;
-  }
 
-  function selectEnclosingWords(editor) {
-    const listSelections = editor.listSelections();
-    console.log("list sel", listSelections);
-    let selections = [];
-    listSelections.forEach((element) => {
-      if (element.anchor.ch === element.head.ch) {
-        const pos = editor.findWordAt(element.anchor);
-        selections.push({
-          anchor: {
-            ch: pos.anchor.ch,
-            line: pos.anchor.line,
-          },
-          head: {
-            ch: pos.head.ch,
-            line: pos.head.line,
-          },
+    function selectEnclosingWords(editor) {
+        const listSelections = editor.listSelections();
+        console.log("list sel", listSelections);
+        let selections = [];
+        listSelections.forEach((element) => {
+            if (element.anchor.ch === element.head.ch) {
+                const pos = editor.findWordAt(element.anchor);
+                selections.push({
+                    anchor: {
+                        ch: pos.anchor.ch,
+                        line: pos.anchor.line,
+                    },
+                    head: {
+                        ch: pos.head.ch,
+                        line: pos.head.line,
+                    },
+                });
+            }
+            selections.push(element);
         });
-      }
-      selections.push(element);
-    });
-    return selections;
-  }
-
-  function modifyPostTransformCursorPos(config, formatted) {
-    config.postTransformCursorPos = formatted
-      ? config.postTransformCursorPos
-      : -config.postTransformCursorPos;
-  }
-
-  function cursorPlacement(editor, nullCheck, config, cursorPosition) {
-    if (nullCheck) {
-      const currentPos = editor.getCursor();
-
-      editor.setCursor(
-        currentPos.line,
-        currentPos.ch - config.postTransformCursorPos
-      );
-    } else {
-      console.log(cursorPosition);
-      let cursorPositions = [];
-      cursorPosition.forEach((element) => {
-        cursorPositions.push({
-          anchor: {
-            line: element.anchor.line,
-            ch: element.anchor.ch + config.postTransformCursorPos,
-          },
-          head: {
-            line: element.anchor.line,
-            ch: element.anchor.ch + config.postTransformCursorPos,
-          },
-        });
-      });
-      editor.setSelections(cursorPositions);
+        return selections;
     }
-  }
 
-  function setFocusOnEditor(editor) {
-    if (!editor.hasFocus()) editor.focus();
-  }
+    function modifyPostTransformCursorPos(config, formatted) {
+        config.postTransformCursorPos = formatted
+            ? config.postTransformCursorPos
+            : -config.postTransformCursorPos;
+    }
 
-  const toggleItalics = {
-    transform(text) {
-      const italicsRegex = /^_((?:.|\r?\n)*?)_$/;
-      return italicsRegex.test(text)
-        ? text.replace(italicsRegex, "$1")
-        : `_${text}_`;
-    },
-    postTransformCursorPos: 1,
-  };
+    function cursorPlacement(editor, nullCheck, config, cursorPosition) {
+        if (nullCheck) {
+            const currentPos = editor.getCursor();
 
-  const toggleBold = {
-    transform(text) {
-      const boldRegex = /^__((?:.|\r?\n)*?)__$/;
-      return boldRegex.test(text)
-        ? text.replace(boldRegex, "$1")
-        : `__${text}__`;
-    },
-    postTransformCursorPos: 2,
-  };
+            editor.setCursor(
+                currentPos.line,
+                currentPos.ch - config.postTransformCursorPos
+            );
+        } else {
+            console.log(cursorPosition);
+            let cursorPositions = [];
+            cursorPosition.forEach((element) => {
+                cursorPositions.push({
+                    anchor: {
+                        line: element.anchor.line,
+                        ch: element.anchor.ch + config.postTransformCursorPos,
+                    },
+                    head: {
+                        line: element.anchor.line,
+                        ch: element.anchor.ch + config.postTransformCursorPos,
+                    },
+                });
+            });
+            editor.setSelections(cursorPositions);
+        }
+    }
 
-  return (
-    <div className="App">
-      <CodeMirror
-        value={text}
-        options={{
-          mode: "markdown",
-          theme: "theme-dark",
-          indentUnit: 4,
-          indentWithTabs: true,
-          tabSize: 4,
-          lineWrapping: true,
-        }}
-        onChange={(value, viewUpdate) => {
-          let str = value.getValue();
-          changeHandler(str);
-        }}
-        ref={data}
-      />
-      <Preview value={text} />
+    function setFocusOnEditor(editor) {
+        if (!editor.hasFocus()) editor.focus();
+    }
 
-      <div>
-        <button
-          onClick={() => {
-            transformer(toggleBold);
-          }}
-        >
-          BOLD
-        </button>
-        <button
-          onClick={() => {
-            transformer(toggleItalics);
-          }}
-        >
-          ITALIC
-        </button>
-        <button
-          onClick={() => {
-            strikethroughText();
-          }}
-        >
-          STRIKETHROUGH
-        </button>
-      </div>
-    </div>
-  );
+    const toggleItalics = {
+        transform(text) {
+            const italicsRegex = /^_((?:.|\r?\n)*?)_$/;
+            return italicsRegex.test(text)
+                ? text.replace(italicsRegex, "$1")
+                : `_${text}_`;
+        },
+        postTransformCursorPos: 1,
+    };
+
+    const toggleBold = {
+        transform(text) {
+            const boldRegex = /^__((?:.|\r?\n)*?)__$/;
+            return boldRegex.test(text)
+                ? text.replace(boldRegex, "$1")
+                : `__${text}__`;
+        },
+        postTransformCursorPos: 2,
+    };
+
+    return (
+        <div className="App">
+            <CodeMirror
+                value={text}
+                options={{
+                    mode: "markdown",
+                    theme: "theme-dark",
+                    indentUnit: 4,
+                    indentWithTabs: true,
+                    tabSize: 4,
+                    lineWrapping: true,
+                }}
+                onChange={(value, viewUpdate) => {
+                    let str = value.getValue();
+                    changeHandler(str);
+                }}
+                ref={data}
+            />
+            <Preview value={text} />
+
+            <div>
+                <button
+                    onClick={() => {
+                        transformer(toggleBold);
+                    }}
+                >
+                    BOLD
+                </button>
+                <button
+                    onClick={() => {
+                        transformer(toggleItalics);
+                    }}
+                >
+                    ITALIC
+                </button>
+                <button
+                    onClick={() => {
+                        strikethroughText();
+                    }}
+                >
+                    STRIKETHROUGH
+                </button>
+            </div>
+        </div>
+    );
 }
