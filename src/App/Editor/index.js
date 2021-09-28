d293cee7e9c4744851fac21ce77f7535d72911ae
Risabh Rai <risabhrai2001@gmail.com>
2021-09-28 08:04:59 +0530

    final styling

--- src/App/Editor/index.js
+++ src/App/Editor/index.js
@@ -5,23 +5,19 @@ import { Controlled as CodeMirror } from "react-codemirror2";
 import "./CodeMirror";
 import CodeMirrorOptions from "./CodeMirror/options";
 
+import { doc } from "firebase/firestore";
+import { ref } from "firebase/storage";
 import {
-	doc
-} from "firebase/firestore";
-import {
-	ref
-} from "firebase/storage";
-import {
-	useAuth,
-	useFirestore,
-	useFirestoreDocData,
-	useStorage,
-	useStorageDownloadURL
+    useAuth,
+    useFirestore,
+    useFirestoreDocData,
+    useStorage,
+    useStorageDownloadURL,
 } from "reactfire";
 
 import {
-	modifyNoteContent,
-	modifyNoteMetadata
+    modifyNoteContent,
+    modifyNoteMetadata,
 } from "../Firebase/firebase-operations";
 
 import Timer from "../Utils/Timer";
@@ -35,174 +31,205 @@ import { useStatus } from "../Utils/StatusIndicator";
 let lastEdited = null;
 
 function Editor({ note, noteID, content }) {
-	const auth = useAuth();
-	const db = useFirestore();
-	const storage = useStorage();
-	const SM = useStatus();
-	
-	useEffect(() => {
-		lastEdited = new Timer(() => { }, 1000);
+    const auth = useAuth();
+    const db = useFirestore();
+    const storage = useStorage();
+    const SM = useStatus();
 
-		return () => {
-			lastEdited = null;
-		};
-	}, []);
+    useEffect(() => {
+        lastEdited = new Timer(() => {}, 1000);
 
-	const [noteName, setNoteName] = useState(note.name);
-	const [tags, setTags] = useState(note.tags);
-	const [tagString, setTagString] = useState(note.tags.join(" "));
-	const [editorText, setEditorText] = useState(content);
+        return () => {
+            lastEdited = null;
+        };
+    }, []);
 
-	const editAccess = auth.currentUser?.uid === note.author;
+    const [noteName, setNoteName] = useState(note.name);
+    const [tags, setTags] = useState(note.tags);
+    const [tagString, setTagString] = useState(note.tags.join(" "));
+    const [editorText, setEditorText] = useState(content);
 
-	const [previewVisible, setPreviewVisible] = useState(true);
+    const editAccess = auth.currentUser?.uid === note.author;
 
-	const CMEditor = useRef(null);
+    const [previewVisible, setPreviewVisible] = useState(true);
 
-	function onNoteNameChange(e) {
-		if (editAccess) {
-			setNoteName(e.target.value);
-		}
-	}
-	function onNoteNameBlur(e) {
-		if (editAccess) {
-			const name = e.target.value.trim();
-			if (name.length > 0 && name.length <= 256 && name !== note.name) {
-				setNoteName(name);
-				SM.add(modifyNoteMetadata(auth, db, storage, noteID, { name }));
-			} else {
-				setNoteName(note.name);
-			}
-		}
-	}
+    const CMEditor = useRef(null);
 
-	function onTagstringChange(e) {
-		const t = e.target.value;
-		setTagString(t);
-		setTags(t.trim().split(/\s+/).filter(s => s.length));
-	}
-	function onTagStringBlur(e) {
-		const t = e.target.value.trim();
-		const tags = t.split(/\s+/).filter(s => s.length);
-		if (t.length > 0 && t.length <= 256 && t !== note.tags.join(" ")) {
-			setTagString(t);
-			setTags(tags);
-			SM.add(modifyNoteMetadata(auth, db, storage, noteID, { tags }));
-		} else {
-			setTagString(note.tags.join(" "));
-			setTags(note.tags);
-		}
-	}
+    function onNoteNameChange(e) {
+        if (editAccess) {
+            setNoteName(e.target.value);
+        }
+    }
+    function onNoteNameBlur(e) {
+        if (editAccess) {
+            const name = e.target.value.trim();
+            if (name.length > 0 && name.length <= 256 && name !== note.name) {
+                setNoteName(name);
+                SM.add(modifyNoteMetadata(auth, db, storage, noteID, { name }));
+            } else {
+                setNoteName(note.name);
+            }
+        }
+    }
 
-	function onEditorChange(editor, data, value) {
-		lastEdited.callback = function () {
-			const t = value;
-			SM.add(modifyNoteContent(auth, db, storage, noteID, {
-				publicAccess: note.publicAccess
-			}, t));
-		};
-		lastEdited.reset().resume();
-		setEditorText(value);
-	}
+    function onTagstringChange(e) {
+        const t = e.target.value;
+        setTagString(t);
+        setTags(
+            t
+                .trim()
+                .split(/\s+/)
+                .filter((s) => s.length)
+        );
+    }
+    function onTagStringBlur(e) {
+        const t = e.target.value.trim();
+        const tags = t.split(/\s+/).filter((s) => s.length);
+        if (t.length <= 256 && t !== note.tags.join(" ")) {
+            setTagString(t);
+            setTags(tags);
+            SM.add(modifyNoteMetadata(auth, db, storage, noteID, { tags }));
+        } else {
+            setTagString(note.tags.join(" "));
+            setTags(note.tags);
+        }
+    }
 
-	if (!editAccess && !note.publicAccess) {
-		return "No permissun, sowwies…";
-	} else return (
-		<div className="note-view">
-			<div className="note-header">
-				<input
-					maxLength="256"
-					disabled={!editAccess}
-					className="title-input"
-					value={noteName}
-					onChange={onNoteNameChange}
-					onBlur={onNoteNameBlur}
-				/>
-				<div className="note-tags">
-					{editAccess && <input
-						maxLength="256"
-						className="tags-input"
-						value={tagString}
-						onChange={onTagstringChange}
-						onBlur={onTagStringBlur}
-					/>}
-					<div className="tags-list">
-						{tags?.map((tag, i) => (<div key={i}>#{tag}</div>))}
-					</div>
-				</div>
-			</div>
-			<div className="toolbar">
-				<Toolbar
-					previewVisibility={[previewVisible, setPreviewVisible]}
-					editorReference={CMEditor}
-					note={note}
-					noteID={noteID}
-					editAccess={editAccess}
-				/>
-			</div>
-			<div className={`note-panel ${previewVisible ? "" : "--preview-hidden"}`}>
-				<div className="note-editor">
-					<CodeMirror
-						value={editorText}
-						options={{
-							readOnly: !editAccess,
-							...CodeMirrorOptions
-						}}
-						onBeforeChange={onEditorChange}
-						ref={CMEditor}
-					/>
-				</div>
-				<div className="note-preview">
-					<Preview value={editorText} />
-				</div>
-			</div>
-		</div>
-	);
+    function onEditorChange(editor, data, value) {
+        lastEdited.callback = function () {
+            const t = value;
+            SM.add(
+                modifyNoteContent(
+                    auth,
+                    db,
+                    storage,
+                    noteID,
+                    {
+                        publicAccess: note.publicAccess,
+                    },
+                    t
+                )
+            );
+        };
+        lastEdited.reset().resume();
+        setEditorText(value);
+    }
+
+    if (!editAccess && !note.publicAccess) {
+        return "No permissun, sowwies…";
+    } else
+        return (
+            <div className="note-view">
+                <div className="note-header">
+                    <input
+                        maxLength="256"
+                        disabled={!editAccess}
+                        className="title-input"
+                        value={noteName}
+                        onChange={onNoteNameChange}
+                        onBlur={onNoteNameBlur}
+                    />
+                    <div className="note-tags">
+                        {editAccess && (
+                            <input
+                                maxLength="256"
+                                className="tags-input"
+                                value={tagString}
+                                onChange={onTagstringChange}
+                                onBlur={onTagStringBlur}
+                            />
+                        )}
+                        <div className="tags-list">
+                            {tags?.map((tag, i) => (
+                                <div key={i}>#{tag}</div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="toolbar">
+                    <Toolbar
+                        previewVisibility={[previewVisible, setPreviewVisible]}
+                        editorReference={CMEditor}
+                        note={note}
+                        noteID={noteID}
+                        editAccess={editAccess}
+                    />
+                </div>
+                <div
+                    className={`note-panel ${
+                        previewVisible ? "" : "--preview-hidden"
+                    }`}
+                >
+                    <div className="note-editor">
+                        <CodeMirror
+                            value={editorText}
+                            options={{
+                                readOnly: !editAccess,
+                                ...CodeMirrorOptions,
+                            }}
+                            onBeforeChange={onEditorChange}
+                            ref={CMEditor}
+                        />
+                    </div>
+                    <div className="note-preview">
+                        <Preview value={editorText} />
+                    </div>
+                </div>
+            </div>
+        );
 }
 
 function ContentLoader({ note, noteID }) {
-	const storage = useStorage();
+    const storage = useStorage();
 
-	const { data: url } = useStorageDownloadURL(ref(storage, `${note.author}/${noteID}.md`));
+    const { data: url } = useStorageDownloadURL(
+        ref(storage, `${note.author}/${noteID}.md`)
+    );
 
-	const [content, setContent] = useState(null);
+    const [content, setContent] = useState(null);
 
-	if (url) {
-		const xhr = new XMLHttpRequest();
-		xhr.responseType = "text";
-		xhr.onload = () => {
-			setContent(xhr.response);
-		};
-		xhr.open("GET", url);
-		xhr.send();
-	}
+    if (url) {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "text";
+        xhr.onload = () => {
+            setContent(xhr.response);
+        };
+        xhr.open("GET", url);
+        xhr.send();
+    }
 
-	if (content !== null) {
-		return <Editor note={note} content={content} noteID={noteID} />
-	} else {
-		return <Loader />;
-	}
+    if (content !== null) {
+        return <Editor note={note} content={content} noteID={noteID} />;
+    } else {
+        return <Loader />;
+    }
 }
 
 export default function EditorLoader({ noteID }) {
-	const auth = useAuth();
-	const db = useFirestore();
+    const auth = useAuth();
+    const db = useFirestore();
 
-	const { status, data: note, error } = useFirestoreDocData(doc(db, "notes", noteID));
+    const {
+        status,
+        data: note,
+        error,
+    } = useFirestoreDocData(doc(db, "notes", noteID));
 
-	switch (status) {
-		case "success": {
-			if (note && (note.publicAccess || auth.currentUser?.uid === note.author)) {
-				return <ContentLoader note={note} noteID={noteID} />;
-			}
-			else return "No permission."
-		}
-		case "loading": {
-			return <Loader />;
-		}
-		case "error": {
-			console.error(error);
-			return "An error has ocurred! Check the console for more details.";
-		}
-	}
+    switch (status) {
+        case "success": {
+            if (
+                note &&
+                (note.publicAccess || auth.currentUser?.uid === note.author)
+            ) {
+                return <ContentLoader note={note} noteID={noteID} />;
+            } else return "No permission.";
+        }
+        case "loading": {
+            return <Loader />;
+        }
+        case "error": {
+            console.error(error);
+            return "An error has ocurred! Check the console for more details.";
+        }
+    }
 }
